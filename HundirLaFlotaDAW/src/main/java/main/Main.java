cf396f29b760a9897b8e0c0c4941e990cb622ced
Miguel Páramos <miguel.paramos@cenecmalaga.es>
2020-02-10 16:20:45 +0100

    Proyecto hundir la flota subido
    Proyecto hundir la flota subido

---
+++ HundirLaFlotaDAW/src/main/java/main/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ * Clase principal del proyecto hundir la flota: Permite jugar
+ * @author Miguel Páramos
+ */
+public class Main {
+
+    /**
+     * función principal, que arranca el juego de hundir la flota.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
