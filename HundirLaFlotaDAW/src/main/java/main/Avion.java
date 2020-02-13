80d5db8cab5f6fcffc4a936972e6d3b40d786e0b
Juanlucg <juanlucg99.jl@gmail.com>
2020-02-13 16:40:53 +0100

    correccion de avion

--- HundirLaFlotaDAW/src/main/java/main/Avion.java
+++ HundirLaFlotaDAW/src/main/java/main/Avion.java
@@ -6,26 +6,43 @@
 package main;
 
 /**
- *
+ * clase del vehiculo de combate avion 
+ * 
  * @author Juanlu
  */
 public class Avion extends VehiculoDeCombate {
-    private boolean direccionMovimiento;
+    private boolean direccionMovimiento;//variable de la direccion del movimiento del avion
     
-    
-    public static void movimientoAvion(Mapa m){
+    /**
+     * 
+     * @param m mapa sobre el que se juega
+     */
+    public void movimientoAvion(Mapa m){
         
     }
-    
+    /**
+     * 
+     * @param posiciones posicion del avion
+     * @param nombre nombre de avion
+     * @param simbolo simbolo del avion
+     * @param estado estado del avion
+     * @param mov movimiento del avion
+     */
     public Avion(CasillaBarco[] posiciones, String nombre,char simbolo,char estado,boolean mov){
         super(posiciones,nombre,simbolo,estado);
         setDireccionMovimiento(mov);
     }
-
+    /**
+     * setter de la direccion del movimiento
+     * @return la direccion del movimiento
+     */
     public boolean isDireccionMovimiento() {
         return direccionMovimiento;
     }
-
+    /**
+     * setter del movimiento del avion a partir de la direccion del movimiento
+     * @param direccionMovimiento direccion del movimiento del avion
+     */
     public void setDireccionMovimiento(boolean direccionMovimiento) {
         this.direccionMovimiento = direccionMovimiento;
     }
