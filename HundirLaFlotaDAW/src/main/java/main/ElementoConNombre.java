063d1eb4ce798cab6593c15ed7bcb3db1ce535ac
Alejanfro <alerd1980@gmail.com>
2020-02-13 15:27:59 +0100

    claseAle

---
+++ HundirLaFlotaDAW/src/main/java/main/ElementoConNombre.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author 2DAW
+ */
+public class ElementoConNombre {
+    /**
+ * Clase ElementoConNombre
+ * Ale Ríos
+ */
+
+    private String nombre;
+
+    public ElementoConNombre(String n){
+        nombre = n;
+    }
+    
+    //Getter
+    public String getNombre(){
+        return nombre;
+    }
+    
+    //Setter
+    public void setNombre(String n){
+        nombre = n;
+    }
+}
+
+
