31d5add51924af9d1853dfec46fe62e0151730b7
noerodrix <57761446+noerodrix@users.noreply.github.com>
2020-02-13 16:48:43 +0100

    Update Partida.java
    Clase comentada.

--- HundirLaFlotaDAW/src/main/java/main/Partida.java
+++ HundirLaFlotaDAW/src/main/java/main/Partida.java
@@ -6,7 +6,7 @@
 package main;
 
 /**
- *
+ * Clase que modela Partida para mi juego hundir la flota.
  * @author nrcif
  */
 public class Partida {
@@ -16,38 +16,62 @@ public class Partida {
     private Jugador jugador1; // Persona 1 del juego
     private Jugador jugador2; // Persona 2 del juego
     
-    // Constructor
+    // Constructor de Partida que tiene en cuenta los turnos de cada jugador.
     public Partida(){
    
     }
     
-    // Getters 
+    /**
+     * Getter del jugador1. 
+     * @return devuelve jugador1.
+     */
    public Jugador getJugador1(){
         return this.jugador1;
     
     }
+   /**
+    * Getter del jugador2.
+    * @return devuelve jugador2.
+    */
     public Jugador getJugador2(){
         return this.jugador2;
     } 
-   
+   /**
+    * Getter del turno que toca en la partida.
+    * @return devuelve el turno de cada jugador.
+    */
     public byte getTurno() {
         return turno;
     }
 
-    // Setters 
+    /**
+     * Setter de jugador1.
+     * @param jugador el jugador1 de la partida.
+     */
      public void setJugador1(Jugador jugador){
         this.jugador1 = jugador;
     }
+     /**
+      * Setter de jugador2.
+      * @param jugador  el jugador2 de la partida.
+      */
     public void setJugador2(Jugador jugador){
         this.jugador2 = jugador;
     }
+    /**
+     * Setter de turno.
+     * @param turno el turno del jugador en la partida.
+     */
 
     public void setTurno(byte turno) {
         this.turno = turno;
     }
 
     
-//  Métodos de clase    
+   /**
+    * Método de la clase Partida creada para cuando finaliza.
+    * @return devuelve true para ----, false para ----.
+    */
     public boolean partidaHaTerminado(){
         return true;
     }
