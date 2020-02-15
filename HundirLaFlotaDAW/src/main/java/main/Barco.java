32e12437e58719d42a27484e153aad54efa7ad7c
XXnikoDarkXX <nicocheto1@gmail.com>
2020-02-15 11:44:49 +0100

    comentario

--- HundirLaFlotaDAW/src/main/java/main/Barco.java
+++ HundirLaFlotaDAW/src/main/java/main/Barco.java
@@ -6,11 +6,17 @@
 package main;
 
 /**
- *
- * @author nicoc
+ *Clase barco donde alojaremos todo lo relacionado con el
+ * @author nico
  */
 public class Barco extends VehiculoDeCombate{
-
+/**
+ * Funcion para meter barco en una casilla
+ * @param posiciones
+ * @param n
+ * @param s simbolo del barco
+ * @param e estado del barco
+ */
     public Barco(CasillaBarco []posiciones, String n,char s,char e){
         super(posiciones,n,s,e);
         
