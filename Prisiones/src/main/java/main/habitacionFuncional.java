6f0e7d9643ba87b3531541d97b90602f76ae6890
Alejanfro <alerd1980@gmail.com>
2020-02-17 15:19:41 +0100

    commit
    comentarios

---
+++ Prisiones/src/main/java/main/habitacionFuncional.java
@@ -0,0 +1,33 @@
+
+/**
+ *Esta es la clase de habitacion funcional que define una habitacion y todo lo relacionado con ella
+ * @author Ale
+ */
+public class HabitacionFuncional extends CosaConNombre{
+    private byte bloque; // Numero de bloque
+    
+    /**
+     * 
+     * @param nombre (nombre habitacion)
+     * @param bloque (numero de bloque)
+     */
+    public HabitacionFuncional (String nombre, byte bloque){
+        super(nombre);
+        this.bloque = bloque;
+    }
+    
+
+    /**
+     * El getter de bloque 
+     * @return devuelve bloque
+     */
+    public byte getBloque() {
+        return bloque;
+    }
+
+ 
+    public void setBloque(byte bloque) {
+        this.bloque = bloque;
+    }
+    
+}
