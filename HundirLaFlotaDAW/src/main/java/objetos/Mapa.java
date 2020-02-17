7f76c20b21ad406226b876234f5cf07283f35f87
Juanlucg <juanlucg99.jl@gmail.com>
2020-02-17 23:38:19 +0100

    creacion de la variable interna disparosRecibidos

--- HundirLaFlotaDAW/src/main/java/objetos/Mapa.java
+++ HundirLaFlotaDAW/src/main/java/objetos/Mapa.java
@@ -19,16 +19,34 @@ public class Mapa {
     private VehiculoDeCombate[] elementos;//vehículo de combate
     private byte nfilas; //número de filas
     private byte ncolumnas; // número de columnas
+    private PosicionMapa[] disparosRecibidos;// disparos recibidos en las posiciones del mapa
 
     /**
      * Descripción constructor Mapa: Se le da el valor a las variables 
      * internas de los setters.
      */
-    public Mapa(VehiculoDeCombate[] elementos, byte nfilas, byte ncolumnas) {
+    public Mapa(VehiculoDeCombate[] elementos, byte nfilas, byte ncolumnas, PosicionMapa[] disparosRecibidos) {
         this.setElementos(elementos);
         this.setNfilas(nfilas);
         this.setNcolumnas(ncolumnas);
+        this.setdisparosRecibidos(disparosRecibidos);
  
+    }
+    
+    /**
+     * getter de disparosRecibidos
+     * @return 
+     */
+    public PosicionMapa[] getDisparosRecibidos() {
+        return disparosRecibidos;
+    }
+    
+    /**
+     * setter de disparosRecibidos
+     * @param disparosRecibidos 
+     */
+    public void setDisparosRecibidos(PosicionMapa[] disparosRecibidos) {
+        this.disparosRecibidos = disparosRecibidos;
     }
       
     //funcion toString
