12a00c6658b514f989c294e7d8b0693c64958d41
Yuri <45168114+YureiH@users.noreply.github.com>
2020-02-20 11:32:16 +0100

    bloque
    getter y setter de bloque

---
+++ Prisiones/src/main/java/main/Bloque.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author M.J.H
+ */
+public class Bloque {
+    private byte nbloque;
+
+        public Bloque(byte b){
+        setBloque(b);
+        
+    }
+    public byte getBloque() {
+      return nbloque;
+   }
+   public void setBloque(byte nbloque) {
+      this.nbloque = nbloque;
+
+    }
+}
